98084be57d0738bbaec56dbc4bcc58f458979ccd
Can Duruk <can@duruk.net>
2011-07-24 02:24:02 -0700

    maybe?

--- public/assets/js/master.js
+++ public/assets/js/master.js
@@ -20,11 +20,11 @@ Lays out events for a single  day
 
 **/
 
-var WIDTH = 60;
+var WIDTH = 600;
 
 var myArray = new Array(721);
 for (var i = 0; i < myArray.length; ++i) {
-  myArray[i] = {total:0, proced: 0};
+  myArray[i] = {total:0, rem: 0};
 }
 
 
@@ -37,36 +37,50 @@ function layOutDay(events) {
 function addEventsToArray(myEvents, timeline) {
   for (var i = 0; i < myEvents.length; ++i) {
     var myEvent = myEvents[i];
-    for (var inner = myEvent.start; inner <= myEvent.end; inner++) {
+    for (var inner = myEvent.start; inner <= myEvent.end; ++inner) {
       timeline[inner].total += 1;
     }
   }
 }
 
 function sweepAndAssign(myEvents, timeline) {
-
   for (var i = 0; i < myEvents.length; ++i) {
-    var conflict = 0,
-        elsOnLeft = 0,
-        myEvent = myEvents[i];
+    var conflict   = 0,
+        myEvent    = myEvents[i],
+        startLevel = timeline[myEvent.start].rem;
 
     for (var inner = myEvent.start; inner < myEvent.end; ++inner) {
       if (conflict < timeline[inner].total) {
         conflict = timeline[inner].total;
       }
-
-      if (elsOnLeft < timeline[inner].proced) {
-        elsOnLeft = timeline[inner].proced;
+      if (timeline[inner].rem === startLevel) {
+        timeline[inner].rem += 1;
       }
     }
 
     myEvent.width = WIDTH / conflict;
-    myEvent.left  = myEvent.width * elsOnLeft;
+    myEvent.left  = myEvent.width * startLevel;
+    myEvent.top   = myEvent.start;
 
-    for (var inner = myEvent.start; inner <= myEvent.end; inner++) {
-      timeline[inner].proced  += 1;
-    }
   }
 }
 
-console.log(layOutDay(input));
+function addThingsToDom() {
+  var realEvents = layOutDay(input);
+  var container = document.getElementById('container');
+
+  for (var i = 0; i < realEvents.length; ++i) {
+    var myEvent   = document.createElement('div'),
+        realEvent = realEvents[i];
+
+    myEvent.style.width  = realEvent.width + 'px';
+    myEvent.style.top    = realEvent.top  + 'px';
+    myEvent.style.left   = realEvent.left  + 'px';
+    myEvent.style.height = (realEvent.end - realEvent.start) + 'px';
+    myEvent.className    = 'event';
+    myEvent.innerHTML   = 'my name is ' + realEvent.id;
+    container.appendChild(myEvent);
+  }
+}
+
+addThingsToDom();
